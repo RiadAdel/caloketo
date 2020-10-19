10d918c2fe7937b36943270752006c143d9fbcec
Riad Adel <riadadel22@gmail.com>
2020-10-19 13:55:06 +0200

    add assets and colors

---
+++ src/config/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+  primary: "#FC6111",
+  secondary: "#11263C",
+  "neutral-100": "#ffffff",
+  "neutral-200": "#ABABBA",
+  "neutral-400": "#52616B",
+  "neutral-300": "#000000",
+};
+
+export default colors;
